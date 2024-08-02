e20f4742e887ac864dea2a4f381298c700206b4e
joacoague15 <joacoague15@gmail.com>
2024-08-02 15:18:36 -0300

    Error handling improved and no hardcoded things anymore

--- diaryapp/views.py
+++ diaryapp/views.py
@@ -1,22 +1,38 @@
 import os
+from urllib.parse import urlparse
 
 import bs4
 
 from langchain import hub
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from openai import OpenAI
+from openai import OpenAI, OpenAIError
 
 from django.conf import settings
 
 
-#link is hardcoded for now
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
 def rag_view(request):
+    web_path = request.GET.get('web_path')
+
+    if web_path is None:
+        return HttpResponseBadRequest('Missing required parameter: web_path')
+
+    if not is_valid_url(web_path):
+        return HttpResponseBadRequest('Invalid URL provided: ' + web_path)
+
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
     os.environ[
         "USER_AGENT"] = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -24,75 +40,111 @@ def rag_view(request):
     os.environ["LANGCHAIN_API_KEY"] = settings.LANGCHAIN_API_KEY
     os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
 
-    loader = WebBaseLoader(
-        web_paths=("https://www.clarin.com/",),
-        bs_kwargs=dict(
-            parse_only=bs4.SoupStrainer(
-                name=("p", "h1", "h2", "h3")
-            )
-        ),
-    )
-    docs = loader.load()
+    try:
+        loader = WebBaseLoader(
+            web_paths=("https://www.clarin.com/",),
+            bs_kwargs=dict(
+                parse_only=bs4.SoupStrainer(
+                    name=("p", "h1", "h2", "h3")
+                )
+            ),
+        )
+        docs = loader.load()
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error loading documents: {str(e)}')
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(docs)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    try:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_documents(docs)
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error splitting documents: {str(e)}')
 
-    retriever = vectorstore.as_retriever()
-    prompt = hub.pull("rlm/rag-prompt")
+    try:
+        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error creating vectorstore: {str(e)}')
 
-    def format_docs(documents):
-        return "\n\n".join(doc.page_content for doc in documents)
+    try:
+        retriever = vectorstore.as_retriever()
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error creating retriever: {str(e)}')
 
-    llm = ChatOpenAI(model="gpt-4o-mini")
+    try:
+        prompt = hub.pull("rlm/rag-prompt")
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error pulling prompt: {str(e)}')
 
-    rag_chain = (
-            {"context": retriever | format_docs, "question": RunnablePassthrough()}
-            | prompt
-            | llm
-            | StrOutputParser()
-    )
+    try:
+        def format_docs(documents):
+            return "\n\n".join(doc.page_content for doc in documents)
 
-    rag_chain_response = rag_chain.invoke(
-        "Puedes darme los principales titulos y novedades que aparecen en la portada de este diario?")
+        llm = ChatOpenAI(model="gpt-4o-mini")
+
+        rag_chain = (
+                {"context": retriever | format_docs, "question": RunnablePassthrough()}
+                | prompt
+                | llm
+                | StrOutputParser()
+        )
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error creating RAG chain: {str(e)}')
+
+    try:
+        rag_chain_response = rag_chain.invoke("Puedes darme los principales titulos y novedades que aparecen en la "
+                                              "portada de este diario?")
+    except Exception as e:
+        return HttpResponseBadRequest(f'Error invoking RAG chain: {str(e)}')
 
     return HttpResponse(rag_chain_response)
 
 
-# rag chain response is hardcoded for now
 def chat_completion_view(request):
+    news_information = request.GET.get('news_information')
+
     client = OpenAI()
 
     responses = []
 
-    lucia_response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system",
-             "content": "Sos Lucía, una persona sumamente sarcástica y con un humor ácido y porteño. Tus comentarios "
-                        "son cortos, directos y sin filtro, con un toque de cinismo y una pizca de argentinidad. Te "
-                        "gusta decir las cosas como son, a veces con un poco de picardía."},
-            {"role": "user", "content": "Venezuela anda en un conflicto muy grande entre su presidente y el pueblo."},
-            {"role": "assistant", "content": ""}
-        ]
-    )
+    try:
+        lucia_response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system",
+                 "content": "Sos Lucía, una persona sumamente sarcástica y con un humor ácido y porteño. Tus "
+                            "comentarios"
+                            "son cortos, directos y sin filtro, con un toque de cinismo y una pizca de argentinidad. Te"
+                            "gusta decir las cosas como son, a veces con un poco de picardía."},
+                {"role": "user",
+                 "content": news_information},
+                {"role": "assistant", "content": ""}
+            ]
+        )
 
-    responses.append(lucia_response.choices[0].message.content)
+        responses.append(lucia_response.choices[0].message.content)
+    except OpenAIError as e:
+        return HttpResponseBadRequest(f'Error creating completion for Lucía: {str(e)}')
+    except Exception as e:
+        return HttpResponseBadRequest(f'Unexpected error creating completion for Lucía: {str(e)}')
 
-    mateo_response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system",
-             "content": "Sos Mateo, una persona sumamente positiva, exageradamente positiva. Ves el lado bueno de "
-                        "todo, incluso en las situaciones mas dificiles y absurdas. No te tomás a vos ni a los demás "
-                        "en serio. Te gusta bromear y hacer comentarios jocosos, incluso sobre temas serios. Podés "
-                        "llegar a ser un poco irritante."},
-            {"role": "user", "content": "Venezuela anda en un conflicto muy grande entre su presidente y el pueblo."},
-            {"role": "assistant", "content": ""}
-        ]
-    )
+    try:
+        mateo_response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system",
+                 "content": "Sos Mateo, una persona sumamente positiva, exageradamente positiva. Ves el lado bueno de "
+                            "todo, incluso en las situaciones mas dificiles y absurdas. No te tomás a vos ni a los "
+                            "demás"
+                            "en serio. Te gusta bromear y hacer comentarios jocosos, incluso sobre temas serios. Podés "
+                            "llegar a ser un poco irritante."},
+                {"role": "user", "content": news_information},
+                {"role": "assistant", "content": ""}
+            ]
+        )
 
-    responses.append(mateo_response.choices[0].message.content)
+        responses.append(mateo_response.choices[0].message.content)
+    except OpenAIError as e:
+        return HttpResponseBadRequest(f'Error creating completion for Mateo: {str(e)}')
+    except Exception as e:
+        return HttpResponseBadRequest(f'Unexpected error creating completion for Mateo: {str(e)}')
 
     return HttpResponse(responses)
-
